48903d0227a32b6a061b0fc7cdf2a6fc87b421e4
João Lucas de Sousa Martins <lucasbanana96@gmail.com>
2023-09-10 19:51:27 -0300

    Create Pages coffee,tea and milk

--- landingpage/urls.py
+++ landingpage/urls.py
@@ -5,5 +5,8 @@ from . import views
 app_name = 'landingpage'
 
 urlpatterns=[
-    path('',views.home, name='home')
+    path('',views.home, name='home'),
+    path('page/coffee',views.coffee, name='coffee'),
+    path('page/tea',views.tea, name='tea'),
+    path('page/milk',views.milk, name='milk')
 ]
--- landingpage/views.py
+++ landingpage/views.py
@@ -5,3 +5,12 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'landingpage/pages/home.html')
+
+def coffee(request):
+    return render(request,'landingpage/pages/coffee.html')
+
+def tea(request):
+    return render(request,'landingpage/pages/tea.html')
+
+def milk(request):
+    return render(request,'landingpage/pages/milk.html')
